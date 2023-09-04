b523a044dc1a6f34ae3bcc30ff61b24ef1cd6e04
Muhammad Shakir <142990310+natasha-maryam@users.noreply.github.com>
2023-09-04 13:11:43 +0500

    Create rti.js

---
+++ RoamToInteger/rti.js
@@ -0,0 +1,48 @@
+let roman = function (s) {
+  function getValue(character) {
+    switch (character) {
+      case "I":
+      case "i":
+        return 1;
+
+      case "V":
+      case "v":
+        return 5;
+
+      case "X":
+      case "x":
+        return 10;
+
+      case "L":
+      case "l":
+        return 50;
+
+      case "C":
+      case "c":
+        return 100;
+
+      case "D":
+      case "d":
+        return 500;
+
+      case "M":
+      case "m":
+        return 1000;
+
+      default:
+        break;
+    }
+  }
+
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (getValue(s[i]) >= getValue(s[i + 1]) || i === s.length - 1) {
+      result += getValue(s[i]);
+    } else {
+      result -= getValue(s[i]);
+    }
+  }
+  return result;
+};
+
+console.log(roman("X"));
